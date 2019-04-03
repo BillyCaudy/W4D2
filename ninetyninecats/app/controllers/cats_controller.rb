c9873643a7ac1a7ba90f3ee30308cc1e9337825c
billycaudy <williamjohncaudy@gmail.com>
2019-04-02 17:24:17 -0700

    Added #edit method to CatsController

--- ninetyninecats/app/controllers/cats_controller.rb
+++ ninetyninecats/app/controllers/cats_controller.rb
@@ -7,4 +7,39 @@ class CatsController < ApplicationController
     @cat = Cat.find_by(id: params[:id])
     render :show
   end
+  def new 
+    @cat = Cat.new
+    render :new
+  end
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      # show user the cat show page
+      redirect_to cat_url(@cat)
+    else
+      # show user the new cat form
+      render :new
+    end
+  end
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+  def update
+    @cat = Cat.find_by(id: params[:id])
+
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+  end
+
 end
+
